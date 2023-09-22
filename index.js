6d4300edb12caa7ef1dbf724f3632d9857e0e205
qobulovasror <qobulovasror0@gmail.com>
2023-09-22 16:46:32 +0500

    bug fix

--- index.js
+++ index.js
@@ -1,35 +1,91 @@
-const express = require('express')
-const fileUpload = require('express-fileupload')
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const qrcode = require("qrcode");
+
+const Jimp = require("jimp");
+const fs = require("fs");
+const qrCodeReader = require("qrcode-reader");
 
 const app = express();
 
 app.use(fileUpload());
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
-app.get('/', (req, res)=>{
-    res.render('index', {status: '', text: ""});
-})
+app.get("/", (req, res) => {
+  res.render("index", {
+    textToCodeResult: "",
+    textToCodeResult: "",
+    codeToDaraResult: "",
+  });
+});
 
-app.post('/', (req, res)=>{
-    if(req.files){
-        const file = req.files.file;
-        const fileName = file.name.replaceAll(' ', '_');
-        
-        file.mv(`${__dirname}/store/${fileName}`, err => {
-            if(err){
-                console.log(err);
-                res.render('index', {status: 'status code: 500', text: err})
-            }else{
-                res.render('index', {status: 'status code: 200', text: 'File successful uploaded'})
-            }
-        })
-    }else{
-        res.render('index', {status: 'status code: 500', text: 'There are no file'})
+app.get("/dataToCode", (req, res) => {
+  const data = req.query.data;
+  if (!data) {
+    res.redirect("/");
+  }
+  qrcode.toDataURL(data, function (err, url) {
+    res.render("index", {
+      textToCodeResult: "",
+      textToCodeResult: url,
+      codeToDaraResult: "",
+    });
+  });
+});
+
+app.post("/upload", (req, res) => {
+  if (req.files) {
+    const file = req.files.file;
+    const fileName = file.name.replaceAll(" ", "_");
+    file.mv(`${__dirname}/img/${fileName}`, (err) => {
+      if (err) {
+        console.log(err);
+        res.redirect('/')
+      } else {
+        res.redirect("/codeToData/"+fileName);
+      }
+    });
+  } else {
+    res.render("error", {
+      errorCode: "404",
+      errorText: "There are no file",
+    });
+  }
+});
+
+app.get("/codeToData/:name", (req, res) => {
+  const buffer = fs.readFileSync('./img/'+req.params.name, (err)=>{
+    if(err)
+        console.log(err);
+        res.redirect('/')
+  });
+  Jimp.read(buffer, function (err, image) {
+    if (err) {
+      console.error(err);
+      res.redirect("/");
     }
-})
+    // Creating an instance of qrcode-reader module
+    let qrCode = new qrCodeReader();
+    qrCode.callback = function (err, value) {
+      if (err) {
+        console.error(err);
+        fs.unlink("./img/" + req.params.name);
+        res.redirect("/");
+      }
+      fs.unlink("./img/" + req.params.name);
+      res.render("index", {
+        textToCodeResult: "",
+        textToCodeResult: '',
+        codeToDaraResult: value.result,
+      });
+    };
+    // Decoding the QR code
+    qrCode.decode(image.bitmap);
+  });
+});
 
-app.listen(5000, ()=>{
-    console.log(`Server running on http://localhost:5000/ `);
-})
+app.listen(5000, () => {
+  console.log(`Server running on http://localhost:5000/ `);
+});
